1c42e69264d5c9d1bb129c030dd6bc257eb003fc
neelra8n <neelratn.official@gmail.com>
2024-06-08 20:59:00 +0530

    update name

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,11 +14,11 @@ const Hero = () => {
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
-        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
+        <h1 className="bold-52 lg:bold-88">Hiking In Himalyas</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
           We want to be on each of your journeys seeking the satisfaction of
           seeing the incorruptible beauty of nature. We can help you on an
-          adventure around the world in just one app
+          adventure around the world in just one app.
         </p>
 
         <div className="my-11 flex flex-wrap gap-5">
@@ -67,13 +67,13 @@ const Hero = () => {
                 height={24}
               />
             </div>
-            <p className="bold-20 text-white">Aguas Calientes</p>
+            <p className="bold-20 text-white">Uttarakhand</p>
           </div>
 
           <div className="flexBetween">
             <div className="flex flex-col">
               <p className="regular-16 block text-gray-20">Distance</p>
-              <p className="bold-20 text-white">173.28 mi</p>
+              <p className="bold-20 text-white">173.28 km</p>
             </div>
             <div className="flex flex-col">
               <p className="regular-16 block text-gray-20">Elevation</p>
