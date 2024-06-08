ffae11efe0a2942e054a7bc9f6f5f2fcde538319
neelra8n <neelratn.official@gmail.com>
2024-06-08 20:38:14 +0530

    hilink vercel deployment

---
+++ components/Navbar.tsx
@@ -0,0 +1,47 @@
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
+
+const Navbar = () => {
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image
+          width={72}
+          height={29}
+          alt="logo"
+          src="/assets/images/hilink-logo.svg"
+        />
+      </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Login"
+          icon="/assets/images/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+      <Image
+        src="/assets/images/menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
+    </nav>
+  );
+};
+
+export default Navbar;
